61ae837b78aa953f37d9fa9ef00eb33ed53bd5d6
Rohan Chakraborty <irohan1999@gmail.com>
2023-07-02 15:30:17 +0530

    feat: update sketch to display gif

--- demo/sketches/box.jsx
+++ demo/sketches/box.jsx
@@ -1,31 +1,29 @@
 export function sketch(p5) {
-  let rotation = 0;
+    let rotation = 0;
+    let image = null;
 
-  p5.setup = () => p5.createCanvas(300, 300, p5.WEBGL);
+    p5.preload = () => {
+        image = p5.loadImage(
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Rotating_earth_%28large%29.gif/300px-Rotating_earth_%28large%29.gif"
+        );
+        image.play();
+    };
+    p5.setup = () => p5.createCanvas(300, 300, p5.WEBGL);
 
-  p5.updateWithProps = props => {
-    if (props.rotation) {
-      rotation = (props.rotation * Math.PI) / 180;
-    }
-  };
+    p5.updateWithProps = props => {
+        if (props.rotation) {
+            rotation = (props.rotation * Math.PI) / 180;
+        }
+    };
 
-  p5.draw = () => {
-    p5.background(100);
-    p5.normalMaterial();
-    p5.noStroke();
+    p5.draw = () => {
+        p5.background(100);
+        p5.normalMaterial();
+        p5.noStroke();
 
-    p5.push();
-    p5.translate(-35, 0);
-    p5.rotateY(rotation);
-    p5.rotateX(-0.9);
-    p5.box(100);
-    p5.pop();
-
-    p5.noFill();
-    p5.stroke(255);
-    p5.push();
-    p5.translate(400, p5.height * 0.35, -200);
-    p5.sphere(300);
-    p5.pop();
-  };
+        p5.push();
+        p5.imageMode(p5.CENTER);
+        p5.image(image, 0, 0, 200, 200);
+        p5.pop();
+    };
 }
